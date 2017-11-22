04f6c2d190acb7861bbfde9a1ebaad808d6901cc
tbaddade <thomas@redaxo.org>
2017-11-22 17:32:51 +0100

    CS

--- gulpfile.js/tasks/init.js
+++ gulpfile.js/tasks/init.js
@@ -4,11 +4,9 @@ const gutil = require('gulp-util');
 const task = (cb) => {
 
     if ('prod' === process.env.APP_ENV) {
-
         console.log(gutil.colors.white('\n  Production\n'));
     }
     else {
-
         console.log(gutil.colors.white('\n  Development\n'));
     }
 
