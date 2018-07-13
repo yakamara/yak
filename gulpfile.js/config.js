48a03b43793af44ad9a1d7a097a3997159e025ee
tbaddade <thomas@redaxo.org>
2018-07-13 10:21:55 +0200

    alphabetische Sortiertung

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -8,39 +8,17 @@ tasks.require('clean').configure({
     paths: './public/assets/{fonts,images,styles,scripts,svgs}/**/*',
 });
 
-tasks.require('styles').configure({
-    source: './assets/styles/*.scss',
-    destination: './public/assets/styles',
-    sassVariables: {
-        $env: process.env.APP_ENV
-    },
-});
-
-tasks.require('scripts').configure({
-    source: './assets/scripts/script.js',
-    destination: './public/assets/scripts',
-});
-
-tasks.require('modernizr').configure({
-    destination: './public/assets/scripts',
-});
-
-tasks.require('svgs').configure({
-    source: './assets/svgs/*.svg',
-    destination: './public/assets/svgs',
-});
-
 const copy = tasks.require('copy');
-// copy.create('fonts', {
-//     source: './assets/fonts',
-//     files: '**/*.{woff,woff2}',
-//     destination: './public/assets/fonts'
-// });
-copy.create('font-awesome', {
-    source: './node_modules/font-awesome/fonts',
-    files: '*.{woff,woff2}',
+copy.create('fonts', {
+    source: './assets/fonts',
+    files: '**/*.{woff,woff2}',
     destination: './public/assets/fonts'
 });
+// copy.create('font-awesome', {
+//     source: './node_modules/font-awesome/fonts',
+//     files: '*.{woff,woff2}',
+//     destination: './public/assets/fonts'
+// });
 
 const images = tasks.require('images');
 images.create('images', {
@@ -51,3 +29,25 @@ images.create('images', {
 //     source: './node_modules/bootstrap-colorpicker/dist/img/bootstrap-colorpicker',
 //     destination: './public/assets/images/bootstrap-colorpicker'
 // });
+
+tasks.require('modernizr').configure({
+    destination: './public/assets/scripts',
+});
+
+tasks.require('scripts').configure({
+    source: './assets/scripts/script.js',
+    destination: './public/assets/scripts',
+});
+
+tasks.require('styles').configure({
+    source: './assets/styles/*.scss',
+    destination: './public/assets/styles',
+    sassVariables: {
+        $env: process.env.APP_ENV
+    },
+});
+
+tasks.require('svgs').configure({
+    source: './assets/svgs/*.svg',
+    destination: './public/assets/svgs',
+});
